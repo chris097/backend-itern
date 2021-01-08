ec2f6291fd7272e7659c98bd019003ebaf119b1b
chris097 <chrisfidel.international@gmail.com>
2021-01-08 08:55:45 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
+const csv = require('csv-parser')
+const fs = require('fs')
+const Joi = require('joi')
 const express = require('express')
 const app = express()
 
-const csv = [
-    { id : 1}
-]
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send(csv)
+const user = []
+
+
+fs.createReadStream('data.csv')
+.pipe(csv({}))
+.on('data', (data) => user.push(data))
+.on('end', () => {
+    console.log(user)
+})
+
+app.get('/api/csv', (req, res) =>{
+    res.send(user)
+})
+
+app.post('/api/csv', (req, res) => {
+
+    const schema = {
+        firstName: Joi.string().min(5).required(),
+        lastName: Joi.string().min(5).required(),
+        age: Joi.number().min(2).required()
+    }
+    const result = Joi.validate(req.body, schema)
+
+    if(result.error){
+       return res.status(400).send(result.error.details[0].message)
+    }
+
+    const item = {
+        conversional_key: user.length + 1,
+        json:[
+            {firstName: req.body.firstName},
+            {lastName: req.body.lastName},
+            {age: req.body.age}
+        ]
+    };
+    user.push(item);
+    res.send(item)
 })
 
 const port = process.env.PORT || 3000
